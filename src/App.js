0ae32c6ce723a19e4252883fdcea90606c192962
MdAkashSheikh <nmakashsheikh@gmail.com>
2023-12-06 11:56:43 +0600

    csv upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Papa from 'papaparse';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState([]);
+    const [column, setColumn] = useState([]);
+    const [values, setValues] = useState([]);
+
+    const handleChange = (e) => {
+        Papa.parse(e.target.files[0], {
+            header: true,
+            skipEmptyLines: true,
+            complete: function(result) {
+                const columnArray = [];
+                const valuesArray = [];
+
+                result.data.map(d => {
+                    columnArray.push(Object.keys(d));
+                    valuesArray.push(Object.values(d));
+                });
+
+                setData(result.data);
+                setColumn(columnArray[0]);
+                setValues(valuesArray);
+            }
+        })
+    }
+
+    console.log("DATA--->", data);
+    console.log('Column--->', column);
+    console.log('Values', values);
+
+    return (
+        <div className="App">
+            <h1>File Upload...</h1>
+            <form>
+                <input type="file" name='file' accept='.csv' onChange={handleChange}/>
+                <input type='submit'/>
+            </form>
+        </div>
+    );
 }
 
 export default App;
